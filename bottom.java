bf8fa70653a27e7ba3e6af122dea8c391da3fcc9
Aaryaman Rattan <107977959+Aaryaman2001@users.noreply.github.com>
2022-07-29 13:53:36 -0400

    Add files via upload

---
+++ bottom.java
@@ -0,0 +1,38 @@
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to return a list containing the bottom view of the given tree.
+    public ArrayList <Integer> bottomView(Node root)
+    {
+        Queue<Pair> q = new ArrayDeque<>();
+        Map<Integer, Integer> map = new TreeMap<>();
+        q.add(new Pair(0, root));
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            map.put(curr.hd, curr.node.data);
+            if(curr.node.left!=null){
+                q.add(new Pair(curr.hd-1, curr.node.left));
+            }
+            if(curr.node.right!=null){
+                q.add(new Pair(curr.hd+1, curr.node.right));
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int entry: map.keySet()){
+            ans.add(map.get(entry));
+        }
+        return ans;
+        
+    }
+    public class Pair{
+        int hd;
+        Node node;
+        Pair(int hd, Node node){
+            this.hd = hd;
+            this.node =node;
+        }
+    }
+}
